e398270df7494b9f936562dbbf7709fcd0a26b56
Michael <mikahado@gmail.com>
2022-10-23 11:45:28 -0400

    commit tree 41b4231704c75ec2f0940560813388c4bf854bb8

--- index.js
+++ index.js
@@ -1,58 +1,14 @@
+//`findMatching`- This function takes an array of drivers' names and a `string`
+// as arguments, and returns the matching list of drivers. The function should be
+// case insensitive.
+
+//
 
 
-function findMatching(drivers, name) {
-    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+function findMatching(drivers, string) {
+
+    const newArray = drivers.filter(driver => driver.toLowerCase() === string)
+
+    return newArray
+
 }
-
-function fuzzyMatch(drivers, name) {
-    return drivers.filter(driver => driver[0] === name[0])
-}
-
-console.log(fuzzyMatch())
-
-function matchName(drivers, name) {
-    return drivers.filter(driver => driver.name === name)
-}
-
-
-
-
-// function findMatching(drivers, attribute) {
-//     return drivers.filter(driver => driver.toLowerCase() === attribute.toLowerCase())
-//   }
-  
-//   function fuzzyMatch(drivers, attribute) {
-//     return drivers.filter(driver => driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
-//   }
-  
-//   function matchName(drivers, argument) {
-//     return drivers.filter(function (driver) { return driver.name === argument })
-//   }
-
-
-
-
-
-
-//descriptive parameter name. review callback functions
-
-// function timesTwo() {
-//     const numbers = ["A", 'B', "C", "D"]
-//     numbers.map(number => number.toLowerCase())
-//     console.log(numbers)
-// }
-
-
-
-
-
-// //     console.log(findMatching())
-
-
-
-
-// return drivers.toLowerCase()
-
-// const fuzzyMatch(drivers, name) = 
-
-// const matchName = 
